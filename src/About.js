8d42138207cb0c62e03ab3662cf1a24cf9e810f0
rasmus petersen <rasm00f1@stud.kea.dk>
2022-01-25 20:30:41 +0100

    first commit

---
+++ src/About.js
@@ -0,0 +1,7 @@
+export default function About(props) {
+  return (
+    <div style={{ backgroundColor: "red" }}>
+      <h1>About</h1>
+    </div>
+  );
+}
---
+++ src/Gallerypopup.js
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+export default function Gallerypopup(props) {
+  return (
+    <div className="popup">
+      <div className="popup_img">
+        <div className="side_by_side">
+          <h2>{props.currentImg.description}</h2>
+          <Link to="/">
+            <h2>X</h2>
+          </Link>
+        </div>
+        <img src={`./img/${props.currentImg.galleryimg}.jpg`} alt="popup version" />
+      </div>
+    </div>
+  );
+}
