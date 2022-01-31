6afef05908f1b2e966b74ad960baa73fe17d65c4
rasmus petersen <rasm00f1@stud.kea.dk>
2022-01-31 16:39:47 +0100

    fixing stuff

--- src/Landing/Review.js
+++ src/Landing/Review.js
@@ -1,7 +1,9 @@
-export default function Review(params) {
+export default function Review(props) {
   return (
-    <div>
+    <div className="review">
       <h2>Reviews</h2>
+      <p>Great game would play again!</p>
+      <p>★★★★☆</p>
     </div>
   );
 }
